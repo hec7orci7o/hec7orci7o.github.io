e5bb6cb9869c68fe65f1933b80530ab7d46900a3
Hector Toral <hectortoraltrabajos@gmail.com>
2024-06-01 16:18:21 +0200

    difficulty

--- src/content/config.ts
+++ src/content/config.ts
@@ -20,7 +20,7 @@ const cheatsheetsCollection = defineCollection({
     author: z.array(z.string()),
     difficulty: z.enum(["Fundamental", "Easy", "Medium", "Hard"]),
     tier: z.number(),
-    type: z.enum(["General", "Defensive","Offensive"]),
+    type: z.enum(["General", "Defensive", "Offensive"]),
     time: z.string(),
     badge: z.string(),
     module: z.string(),
@@ -41,10 +41,12 @@ const walkthroughsCollection = defineCollection({
     releaseDate: z.date(),
     pwnDate: z.date(),
     achievement: z.string().url().nullable(),
+    user: z.array(z.number()),
+    root: z.array(z.number())
   }),
 });
 
-export const collections = { 
+export const collections = {
   activities: logCollection,
   cheatsheets: cheatsheetsCollection,
   walkthroughs: walkthroughsCollection,
